bac6612f2a61e5075db78ab33c1a85953c5867ff
P <p@pop-os.localdomain>
2021-01-22 15:59:18 +0700

    add sign in,sign out button

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,10 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const GoogleAuth = () => {
   const [status, setStatus] = useState(null);
 
+  // variable for Google OAuth Instance
+  const authRef = useRef(null);
+  //input function for checking if state change (for listen function)
   const onAuthChange = (isSignedIn) => {
     setStatus(isSignedIn);
+  };
+
+  //event to sign in
+  const signIn = () => {
+    authRef.current.signIn();
+  };
+
+  const signOut = () => {
+    authRef.current.signOut();
   };
 
   useEffect(() => {
@@ -17,9 +29,9 @@ const GoogleAuth = () => {
             "395460103067-0sas7phd1tod9m0qukf5nig36i7leb6o.apps.googleusercontent.com",
           scope: "email",
         });
-        const auth = window.gapi.auth2.getAuthInstance();
-        setStatus(auth.isSignedIn.get());
-        auth.isSignedIn.listen(onAuthChange);
+        authRef.current = window.gapi.auth2.getAuthInstance();
+        setStatus(authRef.current.isSignedIn.get());
+        authRef.current.isSignedIn.listen(onAuthChange);
       });
     };
     authenticate();
@@ -27,11 +39,21 @@ const GoogleAuth = () => {
 
   const renderAuthButton = () => {
     if (status === null) {
-      return <div>Not sign in</div>;
+      return <button className="ui red loading button">Loading</button>;
     } else if (status) {
-      return <div>I'm sign in</div>;
+      return (
+        <button className="ui red google button" onClick={signOut}>
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>sign in fail</div>;
+      return (
+        <button className="ui red google button" onClick={signIn}>
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   };
   return <div>{renderAuthButton()}</div>;
