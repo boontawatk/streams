8319b1f03fe4bca0cdebf9c49f1a57b50e80af74
P <p@pop-os.localdomain>
2021-01-24 15:53:33 +0700

    add form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCrreate</div>;
+const StreamCreate = (props) => {
+  console.log(props);
+  const onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+  const renderInput = ({ input, label }) => {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input type="text" {...input} />
+      </div>
+    );
+  };
+  return (
+    <form className="ui form" onSubmit={props.handleSubmit(onSubmit)}>
+      <Field name="title" component={renderInput} label="Enter title" />
+      <Field
+        name="description"
+        component={renderInput}
+        label="Enter Description"
+      />
+      <button className="ui button primary">submit</button>
+    </form>
+  );
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
