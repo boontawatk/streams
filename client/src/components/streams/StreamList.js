c447cd328fe5b5fa509d2379f365055463326073
P <p@pop-os.localdomain>
2021-01-25 22:01:34 +0700

    add stream list

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+const StreamList = (props) => {
+  console.log(props.streams);
+  useEffect(() => {
+    props.fetchStreams();
+  }, []);
+  const renderList = () => {
+    return props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  };
+  return (
+    <div>
+      <h2>Streams</h2>
+      <div className="ui celled list">{renderList()}</div>
+    </div>
+  );
 };
 
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = (state) => {
+  //just change object in streams to array of values
+  return { streams: Object.values(state.streams) };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
